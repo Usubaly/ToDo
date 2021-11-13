94d53eb582b0e33463fd55f24d517c2121a82e15
Usubaly Mambetov <usubaly.mambetov@gmail.com>
2021-11-13 11:29:56 +0600

    first commit

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
+
 def homepage(request):
-    return HttpResponse("hello world!")
+    return render(request, "index.html")
 
 
 def test(request):
     return render(request, "test.html")
+
+
+def second(request):
+    return HttpResponse("test 2 page")
